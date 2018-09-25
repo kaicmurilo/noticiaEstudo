844065cf196412d82a0d08452161750df6e40070
kaicmurilo <kaicmurilo@live.com>
2018-09-25 15:00:57 -0400

    commit inicial estudo

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req,res) {
+	res.render("home/index");
+	console.log("Pagina Inicial");
+	// body...
+});
+
+app.get('/formulario_inclusao_noticia', function(req,res) {
+	console.log("Inclusao de noticia");
+	res.render("admin/form_add_noticia");
+	// body...
+});
+
+app.get('/noticias', function(req,res) {
+	console.log("Noticias");
+	res.render("noticias/noticias");
+	// body...
+});
+
+app.get('/tecnologia', function(req,res) {
+	console.log("tecnologia");
+	res.render("secao/tecnologia");
+	// body...
+});
+
+app.listen(3000,function() {
+	console.log("Servidor rodando com express");
+	// body...
+} );
